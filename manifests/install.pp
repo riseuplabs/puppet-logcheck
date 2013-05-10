ebf00192e359a53b59ec5971c4de04142138ad0f
Micah Anderson <micah@riseup.net>
2013-05-10 15:45:02 -0400

    fix file resource, and include owner/modes for logcheck.conf

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,18 +2,26 @@ class logcheck::install ( $version = 'installed' )
 {
 
   include logcheck::params
- 
+
   $packages = $logcheck::params::packages
 
   package { $packages :
     ensure => $version
   }
 
-  '/var/lock/logcheck':
-    ensure  => directory,
-    mode    => '0755',
-    owner   => logcheck,
-    group   => logcheck,
-    require => [ Package['logcheck'], Package['logcheck-database'] ];
+  # ensure certain modes/ownership
+  file {
+    '/var/lock/logcheck':
+      ensure  => directory,
+      mode    => '0755',
+      owner   => logcheck,
+      group   => logcheck,
+      require => [ Package['logcheck'], Package['logcheck-database'] ];
+
+    '/etc/logcheck/logcheck.conf':
+      mode    => '0640',
+      owner   => root,
+      group   => logcheck;
+  }
 
 }
